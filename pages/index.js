698ed49b797080873decb60ad36f050e2778c979
kalwasWe3 <60139437+kalwasWe3@users.noreply.github.com>
2021-09-25 18:28:11 +0200

    Pulling in data by fetching from an API

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 // for next.js's <head> tag and rendering images
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
 // import the web3 library with setup from lib/web3.js
-import { web3 } from '../lib/web3';
+import { web3 } from "../lib/web3";
 
 // import react hooks
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // all from our components folder
-import Account from '../components/Account'
-import EthName from '../components/EthName'
-import Answer from '../components/Answer'
-import AnswerForm from '../components/AnswerForm'
+import Account from "../components/Account";
+import EthName from "../components/EthName";
+import Answer from "../components/Answer";
+import AnswerForm from "../components/AnswerForm";
 
 export default function Home() {
   // todo:
@@ -22,22 +22,41 @@ export default function Home() {
   // 4. make the user name look good
   // 5. let the user post their own reply
 
-const [accounts, setAccounts] = useState([]);
-const [isLogedIn, setisLogedIn] = useState(false);
+  const [accounts, setAccounts] = useState([]);
+  const [isLogedIn, setisLogedIn] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [answers, setAnswers] = useState([]);
 
+  const connect = async function () {
+    let a = await window.ethereum.request({ method: "eth_requestAccounts" });
+    setAccounts(a);
+  };
 
-const connect = async function (){
-  let a =  await window.ethereum.request({method: "eth_requestAccounts"});
-  setAccounts(a);
+  useEffect(
+    function () {
+      if (accounts.length > 0) {
+        setisLogedIn(true);
+      } else {
+        setisLogedIn(false);
+      }
+    },
+    [accounts]
+  );
 
-  if(a.lenght > 0){
-    setisLogedIn(true)
-  }else{
-    setisLogedIn(false)
-  }
+  useEffect(async function () {
+    let a = await window.ethereum.request({ method: "eth_accounts" });
+    setAccounts(a);
 
+    window.ethereum.on("accountsChanged", function (a) {
+      setAccounts(a);
+    });
 
-}
+    fetch("/api/answers")
+      .then((response) => response.json())
+      .then((data) => {
+        setAnswers(data.answers);
+      });
+  }, []);
 
   return (
     <main>
@@ -47,14 +66,20 @@ const connect = async function (){
         <form>
           <input type="text" placeholder="Search" />
         </form>
-        <Account accounts={accounts} isLogedIn={isLogedIn} connect={connect}/>
+        <Account accounts={accounts} isLogedIn={isLogedIn} connect={connect} />
       </header>
 
       <section className="question">
         <div className="main">
           <h3>Feedback forum</h3>
           <h2>Looking for feedback as a beginner</h2>
-          <p>Hey everyone, I&apos;m a new potter, just 4 weeks into my journey, and I&apos;m looking to get some feedback on what I&apos;ve made so far. I&apos;m particularly interested in how to make rustic looking bowls and pots, and I&apos;d love to know what the best tools to use would be!</p>
+          <p>
+            Hey everyone, I&apos;m a new potter, just 4 weeks into my journey,
+            and I&apos;m looking to get some feedback on what I&apos;ve made so
+            far. I&apos;m particularly interested in how to make rustic looking
+            bowls and pots, and I&apos;d love to know what the best tools to use
+            would be!
+          </p>
 
           <div className="slides">
             <Image src="/image-1.jpg" width="600" height="800" />
@@ -64,30 +89,40 @@ const connect = async function (){
           </div>
         </div>
         <div className="meta">
-          
           {/* EthName */}
           <div className="eth-name">
-            <img src="https://ipfs.io/ipfs/QmbctVN8tPaDLiLysVDwThf7JTJhMejbSypZ4a3v5H2G3a" alt="Avatar of riklomas.eth" />
+            <img
+              src="https://ipfs.io/ipfs/QmbctVN8tPaDLiLysVDwThf7JTJhMejbSypZ4a3v5H2G3a"
+              alt="Avatar of riklomas.eth"
+            />
             <div className="name">
               <span className="primary">riklomas.eth</span>
               <span className="secondary">0xb25bf3...aaf4</span>
             </div>
           </div>
           {/* end EthName */}
-
         </div>
       </section>
 
       <section className="answers">
+        {answers.length}
         <div className="loading">Loading answers...</div>
       </section>
 
       <Head>
-        <title>Looking for feedback as a beginner – Feedback forum – Potstop </title>
-        <meta property="og:title" content="Looking for feedback as a beginner on Potstop" />
-        <meta property="og:description" content="This is a project on the SuperHi Crypto + Web3 for Creatives course" />
+        <title>
+          Looking for feedback as a beginner – Feedback forum – Potstop{" "}
+        </title>
+        <meta
+          property="og:title"
+          content="Looking for feedback as a beginner on Potstop"
+        />
+        <meta
+          property="og:description"
+          content="This is a project on the SuperHi Crypto + Web3 for Creatives course"
+        />
         <meta property="og:image" content="/social.png" />
       </Head>
     </main>
-  )
+  );
 }
